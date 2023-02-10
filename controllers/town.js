85dc6c7b57bdeec0de401822f422e250c08143f1
David Sword <vexiconlieus@gmail.com>
2023-02-10 12:41:46 -0700

    Adding PUT and DEL, data validation

--- controllers/town.js
+++ controllers/town.js
@@ -2,44 +2,135 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId; 
 let updateId;
 
+const validateTowns = function(data){
+    //Check for empty inputs
+    if(!data.townName || !data.population || !data.description || !data.leader || !data.climate || !data.specialty || !data.government){
+        return false;
+    }
+    if(isNaN(parseInt(data.population))){
+        console.log(false);
+        return false;
+    }
+    return true;
+}
+
 const displayTowns = async (req, res, next) => {
-    const queryId = req.query.id;
-    const dbresult = await mongodb.getDb().db().collection('towns').find();
-    const dbresultArray = dbresult.toArray();
-    dbresultArray.then((content) => {
-        res.setHeader('Content-Type', 'application/json');
-        if(queryId){
-            const element = content.filter(contact => contact._id.toString() == queryId);
-            res.status(200).json(element);
-        } else {
-            res.status(200).json(content);
-        }
-        
-    })
+    try{
+        const queryId = req.query.id;
+        const dbresult = await mongodb.getDb().db().collection('towns').find();
+        const dbresultArray = dbresult.toArray();
+        dbresultArray.then((content) => {
+            res.setHeader('Content-Type', 'application/json');
+            if(queryId){
+                const element = content.filter(contact => contact._id.toString() == queryId);
+                res.status(200).json(element);
+            } else {
+                res.status(200).json(content);
+            }
+        })
+    }catch(err){
+        res.status(500).send();
+    }
+    
 }
 
 const displayInventory = async (req, res, next) => {
-    const queryId = req.query.id;
-    const dbresult = await mongodb.getDb().db().collection('inventory').find();
-    const dbresultArray = dbresult.toArray();
-    dbresultArray.then((content) => {
-        res.setHeader('Content-Type', 'application/json');
-        if(queryId){
-            const element = content.filter(contact => contact._id.toString() == queryId);
-            res.status(200).json(element);
-        } else {
-            res.status(200).json(content);
-        }
-        
-    })
+    try{
+        const queryId = req.query.id;
+        const dbresult = await mongodb.getDb().db().collection('inventory').find();
+        const dbresultArray = dbresult.toArray();
+        dbresultArray.then((content) => {
+            res.setHeader('Content-Type', 'application/json');
+            if(queryId){
+                const element = content.filter(contact => contact._id.toString() == queryId);
+                res.status(200).json(element);
+            } else {
+                res.status(200).json(content);
+            }
+            
+        })
+    }catch(err){
+        res.status(500).send();
+    }
+    
 }
 
 const addTown = async (req, res, next) => {
-    const collection = await mongodb.getDb().db().collection('towns')
-    collection.insertOne(req.body).then(result => {
-        res.status(201).send(result);
-    }).catch(err => res.status(500).send());
+    try{
+        const collection = await mongodb.getDb().db().collection('towns');
+        const data = {
+            townName: req.body.townName,
+            population: req.body.population,
+            description: req.body.description,
+            leader: req.body.leader,
+            climate: req.body.climate,
+            specialty: req.body.specialty,
+            government: req.body.government
+            
+        }
+        if(!validateTowns(data)){
+            console.log("error");
+            res.status(500).send();
+        }
+        collection.insertOne(req.body).then(result => {
+            res.status(201).send(result);
+        }).catch(err => {console.log(err); res.status(500).send();});
+    }catch(err){
+        console.log(err);
+        res.status(500).send();
+    }
+    
+}
+
+const updateTown = async (req, res, next) => {
+    try{
+        const collection = await mongodb.getDb().db().collection('towns');
+        if(!req.query.id){
+            console.log("Missing ID");
+            res.status(500).send();
+        }
+        if(!validateTowns(req.body)){
+            console.log("Missing data");
+            res.status(500).send();
+        }
+        const user_id = new ObjectId(req.query.id);
+        collection.updateOne({"_id": user_id}, 
+            {$set: {
+                townName: req.body.townName,
+                population: req.body.population,
+                description: req.body.description,
+                leader: req.body.leader,
+                climate: req.body.climate,
+                specialty: req.body.specialty,
+                government: req.body.government
+                
+            }}
+            ).then(result => {
+                res.status(204).send();
+        }).catch(err => {console.log(err); res.status(500).send();});
+    } catch(err){
+        res.status(500).send();
+    }
+
+    
+}
+
+const deleteTown = async (req, res) => {
+    try{
+        const collection = await mongodb.getDb().db().collection('towns');
+        if(!req.query.id){
+            res.send(500).send("Missing ID to delete town");
+        }
+        const user_id = new ObjectId(req.query.id);
+        collection.deleteOne({"_id": user_id}, true
+            ).then(result => {
+                res.status(204).send();
+        }).catch(err => {console.log(err); res.status(500).send();});
+    }catch(err){
+        res.status(500).send();
+    }
+    
 }
 
 
-module.exports = {displayTowns, displayInventory, addTown};
+module.exports = {displayTowns, displayInventory, addTown, updateTown, deleteTown};
--- routes/town.js
+++ routes/town.js
@@ -7,9 +7,8 @@ const swaggerDocument = require('../swaggerTown.json');
 route.get("/towns/", controller.displayTowns);
 route.get("/inventory/", controller.displayInventory);
 route.post("/towns/addTown/", controller.addTown);
-/*route.get("/contacts/update/", controller.updateContactView);
-route.put("/contacts/updateContact/", controller.updateContact);
-route.delete("/contacts/delete/", controller.deleteContact);*/
+route.put("/towns/updateTown/", controller.updateTown);
+route.delete("/towns/deleteTown/", controller.deleteTown);
 
 route.use('/api-docs', swaggerUi.serve);
 route.get('/api-docs', swaggerUi.setup(swaggerDocument));
