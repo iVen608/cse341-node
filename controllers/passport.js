593318075eef4838552a1e53187ff50abbe972f0
David Sword <vexiconlieus@gmail.com>
2023-02-17 20:46:06 -0700

    Testing absolute path

--- controllers/passport.js
+++ controllers/passport.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 passport.use(
     new googleStrat({
-        callbackURL: '/google/redirect',
+        callbackURL: 'https://cse341wintersword.onrender.com/google/redirect',
+        //callbackURL: '/google/redirect',
         scope: ['profile', 'email'],
         clientID: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET
