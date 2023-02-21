5095e421f17bac48297fb78cebe79f7b4910d2ec
David Sword <vexiconlieus@gmail.com>
2023-02-21 12:38:39 -0700

    Added express-session

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser= require('body-parser');
 const app = express();
+const session = require('express-session')
 const destination = require("./routes/")
 const mongodb = require('./db/connect.js');
 const port = process.env.PORT || 3000;
@@ -14,6 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
     next();
   }).use(bodyParser.json())
   .use("/", destination2)
+  .use(session({
+    secret: 'testing',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: true}
+  }))
   .use(passport.session());
 
 const callback = (error) => {
